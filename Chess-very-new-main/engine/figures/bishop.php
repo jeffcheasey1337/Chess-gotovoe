57a5553a6d1ff972060e12d60d41594b0201b2e7
jeffcheasey1337 <90761999+jeffcheasey1337@users.noreply.github.com>
2023-12-12 23:40:15 +0300

    Delete Chess-very-new-main directory

--- Chess-very-new-main/engine/figures/bishop.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Bishop extends Figure {
-    const SHIFTS = array(array(-1, -1), array(-1, 1), array(1, -1), array(1, 1));
-
-    public function getCandidateMoves() {
-        return $this->getLongRangeCandidateMoves(self::SHIFTS);
-    }
-}
--- Chess-very-new-main/make_computer_move.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('engine/chess_game.php');
-$game = new ChessGame();
-$game->loadGame();
-$game->makeComputerMove();
-$data = $game->getClientJsonGameState();
-header('Content-Type: application/json');
-echo json_encode($data, JSON_UNESCAPED_UNICODE);
